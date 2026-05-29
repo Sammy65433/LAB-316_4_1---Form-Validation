22b69efb36da7324631926c3ede3e74d1428e9bc
Sammy65433 <samiamlistening@gmail.com>
2026-05-28 22:40:33 -0400

    ALAB 316.4.1 - Form Validation update 8

--- LAB 316_4_1FV.js
+++ LAB 316_4_1FV.js
@@ -276,9 +276,9 @@ loginForm.addEventListener('submit', e => {
 
     // look thru stored users and give me user whose username 
     // matches person entered
-    const user = users.find(u => 
+    // const user = users.find(u => 
         // u = user obj - check if username = usernameRaw
-        u.username === usernameRaw);
+        // u.username === usernameRaw);
 
     if (!user) { //undefined 
         showError('Username does not exist.', loginForm.elements.username);
@@ -298,64 +298,16 @@ loginForm.addEventListener('submit', e => {
 
 
 
-// Registration Form - Form Submission:
 
-// Usually, we would send this information to an external API for processing. In our case, we are going to process and store the data locally for practice purposes.
 
-// If all validation is successful, store the username, email, and password using localStorage.
-
-// If you are unfamiliar with localStorage, that is okay! Reference the documentation's "Description" and "Examples" sections to learn how to implement it. If you run into issues speak with a peer or one of your instructors.
-
-// Consider how you want to store the user data, keeping in mind that there will be quite a few users registering for the site. Perhaps you want to store it with an array of user objects; or maybe an object whose keys are the usernames themselves.
-
-// Valid usernames should be converted to all lowercase before being stored.
-
-// Valid emails should be converted to all lowercase before being stored.
-
-// Clear all form fields after successful submission and show a success message.
-
-// Registration Form - Username Validation (Part Two):
-
-// Now that we are storing usernames, create an additional validation rule for them...
-
-// Usernames must be unique ("that username is already taken" error).
-//  Remember that usernames are being stored all lowercase, so
-// "learner" and "Learner" are not unique.
-
-// Part 4: Login Form Validation Requirements
-
-// For the Login Form section of the page, implement the following validation requirements:
-
-// Login Form - Username Validation:
-
-// The username cannot be blank.
-
-// The username must exist (within localStorage). Remember that usernames are stored in all lowercase, but the username field accepts (and should not invalidate) mixed-case input.
-
-// Login Form - Password Validation:
-
-// The password cannot be blank.
-
-// The password must be correct (validate against localStorage).
 
 // Login Form - Form Submission:
-
 // If all validation is successful, clear all form fields and show a success message.
-
-// If "Keep me logged in" is checked, modify the success message to
-// indicate this (normally, this would be handled by a variety of
-// persistent login tools and technologies).
-
+// If "Keep me logged in" is checked, modify the success message to indicate this (normally, this would be handled by a variety of persistent login tools and technologies).
 // Part 5: Completion
-
 // Test your validation thoroughly! Try to break things!
-
 // Remember that each successful registration should be stored; therefore you should be able to login with a variety of account credentials.
-
 // When you are done testing your own code, swap sandboxes with a partner and test theirs!
-
 // When each of you are finished testing, share your results.
-
 // Discuss with your partner the differences and similarities between your two approaches. Remember that there is rarely a strictly "correct" or "incorrect" way to solve a problem in development, but there are (almost always) more efficient approaches!
-
 // Remember to submit the link to your finished sandbox using the submission instructions included at the beginning of this document.
